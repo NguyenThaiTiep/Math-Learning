647d2c5b37abb243739d60fa3cc482beaaf63d7c
Le Quang Duy <leqduyvp@gmail.com>
2020-05-18 22:34:47 +0700

    Chỉnh sửa các thông số  hard code thành các biến môi trường, hỗ  trợ triển khai ứng dụng lên các nền tảng như aws, heroku

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
-const express = require('express');
-var bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser');
-var app = express()
+const express = require("express");
+var bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+var app = express();
 
-const db = require("./Database/database")
-const userRoute = require('./routes/userRouter')
-const ScreenRoute = require('./routes/ScreenRouter')
-const AuthRoute = require('./routes/auth')
+const userRoute = require("./routes/userRouter");
+const ScreenRoute = require("./routes/ScreenRouter");
+const AuthRoute = require("./routes/auth");
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs')
-app.set('views', './views')
-app.set(express.static('public'))
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.set(express.static("public"));
 
+app.use("/users", userRoute);
+app.use("/screen", ScreenRoute);
 
-app.use('/users', userRoute)
-app.use('/screen', ScreenRoute)
+app.use("/public", express.static("./public"));
+app.use("/", AuthRoute);
 
-app.use('/public', express.static('./public'))
-app.use('/', AuthRoute);
-
-app.listen(port, function() {
-    console.log("Server is listening port " + port);
-
-})
+app.listen(port, function () {
+  console.log("Server is listening port " + port);
+});
